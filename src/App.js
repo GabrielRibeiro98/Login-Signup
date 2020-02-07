8f38dd0c70564953da38a436e554e86fc876efdc
Gabriel Ribeiro <gabrielribeiro1198@gmail.com>
2020-02-07 11:55:34 -0300

    Creating the interface

--- src/App.js
+++ src/App.js
@@ -4,7 +4,27 @@ import './App.css';
 export default function App(){
   
   return (
-    <div>
+    <div class="grid-container">
+      <div class="grid-visual"></div>
+      <div class="grid-login">
+        <h1>Login</h1>
+        <div class="login">
+          <h2>be welcome</h2>
+          <form>
+            <input type="text" class="input-username" placeholder="username"/>
+            <input type="text" class="input-password"placeholder="password"/>
+            <input type="checkbox" name="remember" class="checkbox"/>
+            <label for="remember" class="remember">remember me</label>
+          </form>
+        </div>
+      </div>
+      <div class="grid-visual-grid">
+        <div class="grid1"></div>
+        <div class="grid2"></div>
+        <div class="grid3"></div>
+        <div class="grid4"></div>
+        <div class="grid5"></div>
+      </div>
     </div>
   );
 }
